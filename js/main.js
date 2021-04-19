90a0300c61635292e1dec4e64ccea25f73c0effa
Brandi Cameron <hello@brandicameron.com>
2021-04-19 14:41:56 -0500

    Added hard level

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const board = document.querySelector(".board");
 const startBtn = document.getElementById("start-btn");
+const toggle = document.getElementById("switch");
 let cardsCurrentlyFlipped = [];
 let score = 0;
 let scoreHistory = [];
 
-let imgArray = [
+let easyArray = [
   {
     src: "../img/banana.svg",
     id: 1,
@@ -67,6 +68,69 @@ let imgArray = [
   },
 ];
 
+let hardArray = [
+  {
+    src: "../img/13.svg",
+    id: 13,
+    name: "13",
+  },
+  {
+    src: "../img/14.svg",
+    id: 14,
+    name: "13",
+  },
+  {
+    src: "../img/15.svg",
+    id: 15,
+    name: "15",
+  },
+  {
+    src: "../img/16.svg",
+    id: 15,
+    name: "15",
+  },
+  {
+    src: "../img/17.svg",
+    id: 17,
+    name: "17",
+  },
+  {
+    src: "../img/18.svg",
+    id: 18,
+    name: "17",
+  },
+  {
+    src: "../img/19.svg",
+    id: 19,
+    name: "19",
+  },
+  {
+    src: "../img/20.svg",
+    id: 20,
+    name: "19",
+  },
+  {
+    src: "../img/21.svg",
+    id: 21,
+    name: "21",
+  },
+  {
+    src: "../img/22.svg",
+    id: 22,
+    name: "21",
+  },
+  {
+    src: "../img/23.svg",
+    id: 23,
+    name: "23",
+  },
+  {
+    src: "../img/24.svg",
+    id: 24,
+    name: "23",
+  },
+];
+
 class MemoryCard {
   constructor(imgSrc, imgName) {
     const board = document.querySelector(".board");
@@ -93,9 +157,19 @@ function startGame() {
   createBoard();
 }
 
+let difficulty = easyArray;
+function determineDifficulty() {
+  if (toggle.checked === false) {
+    difficulty = easyArray;
+  } else {
+    difficulty = hardArray;
+  }
+  startGame();
+}
+
 function createBoard() {
   // randomizes the array https://flaviocopes.com/how-to-shuffle-array-javascript/
-  let randomImgArray = imgArray.sort(() => Math.random() - 0.5);
+  let randomImgArray = difficulty.sort(() => Math.random() - 0.5);
   randomImgArray.forEach((img) => {
     new MemoryCard(img.src, img.name);
   });
@@ -161,3 +235,4 @@ startGame();
 // Event Listeners
 startBtn.addEventListener("click", startGame);
 board.addEventListener("click", flipCards);
+toggle.addEventListener("change", determineDifficulty);
