3fcaa1ffe743b4e371fc08875d1e83ac96cd6c16
Brandi Cameron <hello@brandicameron.com>
2021-05-27 12:45:59 -0500

    fixed img paths

--- js/easy-array.js
+++ js/easy-array.js
@@ -1,61 +1,61 @@
 const easyArray = [
   {
-    src: "../img/banana.svg",
+    src: "./img/banana.svg",
     id: 1,
     name: "banana",
   },
   {
-    src: "../img/banana.svg",
+    src: "./img/banana.svg",
     id: 2,
     name: "banana",
   },
   {
-    src: "../img/cake.svg",
+    src: "./img/cake.svg",
     id: 3,
     name: "cake",
   },
   {
-    src: "../img/cake.svg",
+    src: "./img/cake.svg",
     id: 4,
     name: "cake",
   },
   {
-    src: "../img/donut.svg",
+    src: "./img/donut.svg",
     id: 5,
     name: "donut",
   },
   {
-    src: "../img/donut.svg",
+    src: "./img/donut.svg",
     id: 6,
     name: "donut",
   },
   {
-    src: "../img/strawberry.svg",
+    src: "./img/strawberry.svg",
     id: 7,
     name: "strawberry",
   },
   {
-    src: "../img/strawberry.svg",
+    src: "./img/strawberry.svg",
     id: 8,
     name: "strawberry",
   },
   {
-    src: "../img/taco.svg",
+    src: "./img/taco.svg",
     id: 9,
     name: "taco",
   },
   {
-    src: "../img/taco.svg",
+    src: "./img/taco.svg",
     id: 10,
     name: "taco",
   },
   {
-    src: "../img/wine.svg",
+    src: "./img/wine.svg",
     id: 11,
     name: "wine",
   },
   {
-    src: "../img/wine.svg",
+    src: "./img/wine.svg",
     id: 12,
     name: "wine",
   },
--- js/hard-array.js
+++ js/hard-array.js
@@ -1,61 +1,61 @@
 const hardArray = [
   {
-    src: "../img/13.svg",
+    src: "./img/13.svg",
     id: 13,
     name: "13",
   },
   {
-    src: "../img/14.svg",
+    src: "./img/14.svg",
     id: 14,
     name: "13",
   },
   {
-    src: "../img/15.svg",
+    src: "./img/15.svg",
     id: 15,
     name: "15",
   },
   {
-    src: "../img/16.svg",
+    src: "./img/16.svg",
     id: 15,
     name: "15",
   },
   {
-    src: "../img/17.svg",
+    src: "./img/17.svg",
     id: 17,
     name: "17",
   },
   {
-    src: "../img/18.svg",
+    src: "./img/18.svg",
     id: 18,
     name: "17",
   },
   {
-    src: "../img/19.svg",
+    src: "./img/19.svg",
     id: 19,
     name: "19",
   },
   {
-    src: "../img/20.svg",
+    src: "./img/20.svg",
     id: 20,
     name: "19",
   },
   {
-    src: "../img/21.svg",
+    src: "./img/21.svg",
     id: 21,
     name: "21",
   },
   {
-    src: "../img/22.svg",
+    src: "./img/22.svg",
     id: 22,
     name: "21",
   },
   {
-    src: "../img/23.svg",
+    src: "./img/23.svg",
     id: 23,
     name: "23",
   },
   {
-    src: "../img/24.svg",
+    src: "./img/24.svg",
     id: 24,
     name: "23",
   },
